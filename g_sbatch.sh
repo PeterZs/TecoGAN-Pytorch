2c8c9fa39e90b9a5a8450375f54673139c73fcb6
qianjinhao <qianjinhao@126.com>
2021-03-29 13:58:58 +0800

    g_sbatch.sh

--- g_sbatch.sh
+++ g_sbatch.sh
@@ -65,7 +65,7 @@ echo "#SBATCH --gres=gpu:$2" >> ${sbatch_file}
 echo "#SBATCH --nodelist=$3" >> ${sbatch_file}
 echo "#SBATCH --job-name=${jobname}" >> ${sbatch_file} 
 echo "#SBATCH -o ${path}/logs/${jobname}/%j.txt" >> ${sbatch_file} 
-echo "srun --mpi=pmi2  python  codes/main.py --exp_dir ${exp_dir}  --mode train --model ${model} --opt train${suffix}.yml --gpu_id ${gpu_id} > ${exp_dir}/train/train${suffix}.log  2>&1 & " >> ${sbatch_file}
+echo "srun --mpi=pmi2  python  codes/main.py --exp_dir ${exp_dir}  --mode train --model ${model} --opt train${suffix}.yml --gpu_id ${gpu_id} " >> ${sbatch_file}
 echo "echo \"Submit the ${jobname} job by run \'sbatch\'\" " >>  ${sbatch_file}
 
 cat sbatch_run.sh
